02882e56ec1b8cc6c3904e6d46eeea71fbace5f6
조성찬 <wordnumperiod@gmail.com>
2023-07-26 13:19:10 +0900

    [BE] Fix: EC2 Docker 컨테이너에서 빌드 안 되는 문제 해결

--- backend/src/main/java/com/dutaduta/sketchme/SketchmeApplication.java
+++ backend/src/main/java/com/dutaduta/sketchme/SketchmeApplication.java
@@ -2,11 +2,13 @@ package com.dutaduta.sketchme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class SketchmeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SketchmeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SketchmeApplication.class, args);
+    }
 }
